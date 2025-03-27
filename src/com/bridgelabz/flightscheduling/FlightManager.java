c4c14ccefb28234542e3a96016be3ccf549037b3
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-27 14:06:23 +0530

    [Sehaj] ADD: Service Class for Managing Flights

--- src/com/bridgelabz/flightscheduling/FlightManager.java
+++ src/com/bridgelabz/flightscheduling/FlightManager.java
@@ -1,7 +1,7 @@
 package com.bridgelabz.flightscheduling;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 public class FlightManager <T> {
     private List<Flight<T>> flights = new ArrayList<>();
