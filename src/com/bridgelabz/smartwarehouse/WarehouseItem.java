a8b5959e870bd3382092c5e0a2846c9f52f0a707
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-27 14:50:59 +0530

    [Sehaj] ADD: Abstract Class for Warehouse Items

--- src/com/bridgelabz/smartwarehouse/WarehouseItem.java
+++ src/com/bridgelabz/smartwarehouse/WarehouseItem.java
@@ -16,3 +16,36 @@ abstract class WarehouseItem {
         return "Item: " + itemName;
     }
 }
+
+class Electronics extends WarehouseItem {
+    public Electronics(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Electronics - " + super.toString();
+    }
+}
+
+class Groceries extends WarehouseItem {
+    public Groceries(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Groceries - " + super.toString();
+    }
+}
+
+class Furniture extends WarehouseItem {
+    public Furniture(String name) {
+        super(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Furniture - " + super.toString();
+    }
+}
