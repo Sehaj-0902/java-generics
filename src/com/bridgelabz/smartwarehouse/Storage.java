3df5f54288445a203d68ae39beb88ed549320e30
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-27 21:56:49 +0530

    [Sehaj] ADD: Generic Class Storage to store items

--- src/com/bridgelabz/smartwarehouse/Storage.java
+++ src/com/bridgelabz/smartwarehouse/Storage.java
@@ -1,24 +1,30 @@
 package com.bridgelabz.smartwarehouse;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class Storage <T extends WarehouseItem > {
-    private List<T> items = new ArrayList<>();
+class Storage<T extends WarehouseItem> {
+    private Map<String, T> items = new HashMap<>();
 
     public void addItem(T item) {
-        items.add(item);
+        items.put(item.getItemId(), item);
     }
 
-    public T getItem(int index) {
-        return items.get(index);
+    public T getItem(String itemId) {
+        return items.get(itemId);
     }
 
-    public List<T> getItems(){
-        return items;
+    public List<T> getItems() {
+        return new ArrayList<>(items.values());
     }
 
-    public int quo() {
+    public int size() {
         return items.size();
+    }
+
+    public void removeItem(String itemId) {
+        items.remove(itemId);
     }
 }
