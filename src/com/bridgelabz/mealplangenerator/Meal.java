fbd796161cd2566b92a680b3e3f058686f744a16
Sehaj-0902 <sehajpreet1296.be21@chitkara.edu.in>
2025-03-28 22:37:18 +0530

    [Sehaj] ADD: Generic Class to handle different meal plans

---
+++ src/com/bridgelabz/mealplangenerator/Meal.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.mealplangenerator;
+
+class Meal<T extends MealPlan> {
+    private T mealPlan;
+
+    public Meal(T mealPlan) {
+        this.mealPlan = mealPlan;
+    }
+
+    public T getMealPlan() {
+        return mealPlan;
+    }
+
+    @Override
+    public String toString() {
+        return mealPlan.getDescription();
+    }
+}
